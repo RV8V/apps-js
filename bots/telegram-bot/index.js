a8fd560438ac77c7038f1f93e3f5f2dd4ead76eb
Ruslan <rv.volovik88@gmail.com>
2020-11-11 17:19:26 +0200

    testing code

--- bots/telegram-bot/index.js
+++ bots/telegram-bot/index.js
@@ -1,6 +1,6 @@
 const telegram_bot = require('node-telegram-bot-api')
 const process = require('process')
-const { debug, lower, flag } = require('./util')
+const { debug, lower, flag, html_format, reference } = require('./util')
 
 process.env.NTBA_FIX_319 = 1
 
@@ -13,6 +13,16 @@ bot.on('message', ({ chat, from, text }) => {
   process.stdout.write(JSON.stringify(from))
   flag ? lower(text) === 'hello' ? (process.stdout.write('enter here\n'), bot.sendMessage(chat.id, 'hello, ' + from.first_name)) : bot.sendMessage(chat.id, debug(chat)) : setTimeout(() => (
     bot.sendMessage(chat.id, 'http://localhost/8008', html_format && reference ? { parse_mode: 'HTML', disable_web_page_preview: false } : { parse_mode: 'Marhdown', disable_notification: true }).then(() => process.stdout.write('\nhi\n')).catch(err => err)), 4000)
+
+
+  text === 'shut down' ? bot.sendMessage(chat.id, 'closing keyboard', { reply_markup: { remove_keyboard: true } }) : 'sending keyboard once more'
+  text === 'answer' ? bot.sendMessage(chat.id, 'answering', { reply_markup: { force_reply: true } }) : 'the same one'
+
+  bot.sendMessage(chat.id, 'keyboard', {
+    reply_markup: {
+      keyboard: [ [{ text: 'send location', request_location: true}], ['answer', 'shut down'], [{ text: 'send contact', request_contact: true }], ['what do you want to see'] ], one_time_keyboard: true
+    }
+  })
 })
 
 bot.onText(/\/start/, ({ chat: { id } }) => bot.sendMessage(id, 'hello'))
