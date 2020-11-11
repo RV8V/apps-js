8618c182fff60e320a577e0b33f0b38f0b4c0116
Ruslan <rv.volovik88@gmail.com>
2020-11-11 16:02:06 +0200

    testing mode, templates

--- bots/telegram-bot/index.js
+++ bots/telegram-bot/index.js
@@ -8,8 +8,13 @@ const TOKEN = '1376866800:AAFQYmpyLsbA1Hkf8DkxlpCSPKFqEZ_nRHQ'
 const bot = new telegram_bot(TOKEN, { polling: { interval: 300, autoStart: true, params: { timeout: 10 } }})
 
 bot.on('message', ({ chat, from, text }) => {
+  const html = `<h2><strong>hello, ${from.first_name}</strong><i>test message</i></h2><hr /><pre>${debug(from)}</pre>`
+  const markdown = `*hello, ${from.first_name}* _Italic spme testing text in md_`
   process.stdout.write(JSON.stringify(from))
-  flag ? lower(text) === 'hello' ? (process.stdout.write('enter here\n'), bot.sendMessage(chat.id, 'hello, ' + from.first_name)) : bot.sendMessage(chat.id, debug(chat)) : (
-    bot.sendMessage(chat.id, 'testing string').then(() => console.log('hi')).catch(err => err)
-  )
+  flag ? lower(text) === 'hello' ? (process.stdout.write('enter here\n'), bot.sendMessage(chat.id, 'hello, ' + from.first_name)) : bot.sendMessage(chat.id, debug(chat)) : setTimeout(() => (
+    bot.sendMessage(chat.id, 'http://localhost/8008', html_format && reference ? { parse_mode: 'HTML', disable_web_page_preview: false } : { parse_mode: 'Marhdown', disable_notification: true }).then(() => process.stdout.write('\nhi\n')).catch(err => err)), 4000)
 })
+
+bot.onText(/\/start/, ({ chat: { id } }) => bot.sendMessage(id, 'hello'))
+
+bot.onText(/\/help (.+)/, ({ chat: { id }, }, [source, match]) => bot.sendMessage(id, debug(match)))
--- bots/telegram-bot/util.js
+++ bots/telegram-bot/util.js
@@ -1,5 +1,7 @@
 module.exports = {
   debug: (obj = {}) => JSON.stringify(obj, null, 4),
   lower: str => str.toLowerCase(),
-  flag: false
+  flag: false,
+  html_format: false,
+  reference: false
 }
