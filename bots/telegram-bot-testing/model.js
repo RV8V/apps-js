4cc75627be90fa50e3132f11fe2ff003329a3e2e
Ruslan <rv.volovik88@gmail.com>
2020-11-12 13:18:04 +0200

    bad code, just for templates

--- bots/telegram-bot-testing/model.js
+++ bots/telegram-bot-testing/model.js
@@ -1,7 +1,5 @@
-const { Schema } = require('mongoose')
-
 module.exports = {
-  film_schema: new Schema({ name: { type: String, required: true }, type: { type: String, required: true }, uuid: { type: String, required: true }, year: { type: String }, rate: { type: Number }, length: { type: Number }, link: { type: String } }),
-  cinema_schema: new Schema({ name: { type: String, required: true }, url: { type: String, required: true }, type: { type: String, required: true }, uuid: { type: String, required: true }, rate: { type: Number }, length: { type: Number }, link: { type: String } }),
-  user_schema: new Schema({ telegram_id: { type: Number, required: true }, films: { type: [String], default: null } })
+  film_schema: require('mongoose').Schema({ name: { type: String, required: true }, type: { type: String, required: true }, uuid: { type: String, required: true }, year: { type: String }, rate: { type: Number }, length: { type: Number }, link: { type: String } }),
+  cinema_schema: require('mongoose').Schema({ name: { type: String, required: true }, url: { type: String, required: true }, type: { type: String, required: true }, uuid: { type: String, required: true }, rate: { type: Number }, length: { type: Number }, link: { type: String } }),
+  user_schema: require('mongoose').Schema({ telegram_id: { type: Number, required: true }, films: { type: [String], default: null } })
 }
