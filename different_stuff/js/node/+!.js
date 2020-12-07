04ec01032d450d565efc1d001758ff5e59082085
Ruslan <rv.volovik88@gmail.com>
2020-12-07 22:31:45 +0200

    some testing features

---
+++ different_stuff/js/node/+!.js
@@ -0,0 +1,27 @@
++function() {
+  (function() {
+    console.log('()')
+  })()
+
+  !function() {
+    console.log('!')
+  }()
+
+  +function() {
+    console.log('+')
+    return 10;
+  }();
+
+  (() => {
+    console.log('()()')
+  })();
+
+  /*!() => {
+    console.log('tes string e')
+  }();*/
+
+  /*+() => {
+    console.log('tes string f')
+  }()*/
+
+}.call(this)
