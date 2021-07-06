41982f03e2e36adc908fc9fedcdefc69fb3e6f4a
rabcloudz <volovyk.r@abcloudz.com>
2021-07-06 20:34:10 +0300

    functional programming

---
+++ miscellaneous/FUNCTIONAL_PROGRAMMING/fp-in-frontend/functor.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const functor = ['one', 'two', 'three']
+const anotherFunctor = functor.map(item => item.length)
+
+const maybe = x => f => x && f && maybe(f(x)) || maybe(null)
+
+maybe(10)(x => x + 3)(console.log)
+
+/**
+ * u.map(x => x) === u - identity
+ * u.map(f).map(g) === u.map(x => g(f(x))) - composition
+ */
