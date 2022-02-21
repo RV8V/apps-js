d7ead0ab95a41b470237bb53e297fece070651f3
RV8V <rv.volovik88@gmail.com>
2022-02-21 21:05:16 +0200

    refactor: change console methods for intospection, console.dir

--- miscellaneous/DIFFERENT_STUFF/monorepository/lerna/packages/server/index.js
+++ miscellaneous/DIFFERENT_STUFF/monorepository/lerna/packages/server/index.js
@@ -1,4 +1,4 @@
 const common = require('@workspaces/common');
 const getMessage = (message) => message;
 
-console.log({ common: getMessage(common().message) });
+console.dir({ common: getMessage(common().message) });
